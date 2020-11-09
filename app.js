506c98ed9ad90fab8c7a1e7144a014d19c3dd506
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2020-11-09 01:21:34 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let boxA = document.querySelector(".box1");
+let boxB = document.querySelector(".box2");
+let gridContainer = document.querySelector(".grid-container");
+
+
+
+function toTealDark(){
+   boxA.style.background = "teal";
+   boxB.style.background = "#333";
+   gridContainer.style.background = "#ccc";
+   gridContainer.style.color = "#333";
+
+}
